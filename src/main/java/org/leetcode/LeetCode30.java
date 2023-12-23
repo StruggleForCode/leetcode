3e307cce11aa45c60fb427d71f874ed41218111b
StuggleForCode <leehao99@163.com>
2023-12-23 13:39:23 +0800

    US202312171942 leetcode 30

--- src/main/java/org/leetcode/LeetCode30.java
+++ src/main/java/org/leetcode/LeetCode30.java
@@ -17,7 +17,13 @@ public class LeetCode30 {
         String[] words = {"bar","foo","the"};
         String s2 = "barfoothefoobarman";
         String[] words2 = {"bar","foo"};
-        List<Integer> substring = findSubstring(s, words);
+        String s3 = "wordgoodgoodgoodbestword";
+        String[] words3 = {"word","good","best","word"};
+        String s4 = "wordgoodgoodgoodbestword";
+        String[] words4 = {"word","good","best","good"};
+        String s5 = "aaaaaaaaaaaaaa";
+        String[] words5 = {"aa","aa"};
+        List<Integer> substring = findSubstring(s5, words5);
         System.out.println(substring);
 
     }
@@ -26,37 +32,26 @@ public class LeetCode30 {
         if (words.length == 0 || s.length() == 0) {
             return res;
         }
-
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            String key = words[i];
-            if (map.containsKey(key)) {
-                int value = map.get(key);
-                map.put(key, value + 1);
-            }
-            map.put(key, 1);
-        }
-
         for (int i = 0; i < words[0].length(); i++) {
-            function(s, words, map, res, i, i);
+            function(s, words, res, i, i);
         }
-
         return res;
     }
 
-    public static void function(String s, String[] words, Map<String, Integer> map, List<Integer> res, int left, int right){
+    public static void function(String s, String[] words, List<Integer> res, int left, int right){
+        // 存到map中
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            String key = words[i];
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
         int width = words[0].length();
+        // size 用于记录 map中的值是否取完
         int size = words.length;
-        while (right < s.length()) {
+        while (right <= s.length()) {
             if (size == 0) {
                 res.add(left);
-                String keyTemp = s.substring(left, left + width);
-                if (map.containsKey(keyTemp)) {
-                    int valueTemp = map.get(keyTemp);
-                    map.put(keyTemp, valueTemp + 1);
-                    size++;
-                }
-                left += width;
             }
             if (right + width > s.length()) break;
             String tmp = s.substring(right, right + width);
@@ -66,27 +61,24 @@ public class LeetCode30 {
                 right += width;
                 size--;
             } else {
-                while (left <= right) {
-                    if (size >= words.length) {
-                        break;
-                    }
-                    String keyTemp = s.substring(left, left + width);
-                    if (map.containsKey(keyTemp)) {
-                        int valueTemp = map.get(keyTemp);
-                        map.put(keyTemp, valueTemp + 1);
-                        size++;
-                    }
+                String keyTemp = s.substring(left, left + width);
+                if (map.containsKey(keyTemp)) {
+                    int valueTemp = map.get(keyTemp);
+                    map.put(keyTemp, valueTemp + 1);
+                    size++;
                     left += width;
+                } else {
+                    // 这一个判断有一点点绕
+                    left += width;
+                    right += width;
                 }
-                if (size >= words.length) {
-                    continue;
-                }
-                right += width;
             }
         }
     }
 }
 
 /*
-barfoofoobarthefoobarman
+bar foo foo bar the foo bar man
+
+"bar","foo","the"
  */
